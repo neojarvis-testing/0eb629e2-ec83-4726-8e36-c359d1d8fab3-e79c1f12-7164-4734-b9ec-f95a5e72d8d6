e07d737b3ba7d2dd1b5fc03e753dcf5c05fac5bc
sasivarunan390 <sasivarunan390@gmail.com>
2025-05-23 11:30:28 +0000

    authservice angular

--- angularapp/src/app/models/requirement.model.ts
+++ angularapp/src/app/models/requirement.model.ts
@@ -1,2 +1,15 @@
 export class Requirement {
+    RequirementId?:number;
+    title: string;
+    description: string;
+    department: string;
+    postedDate: Date;
+    status: string;
+    duration: string;
+    mode: string;
+    location: string;
+    skillLevel: string;
+    budget: string;
+    priority: string;
+    trainerId?: number;
 }
