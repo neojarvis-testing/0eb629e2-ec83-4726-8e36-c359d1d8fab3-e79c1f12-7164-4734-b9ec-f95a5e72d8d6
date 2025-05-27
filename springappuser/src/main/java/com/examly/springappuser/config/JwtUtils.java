ced4b3e9086334800141a0cbcea1d3788c9b4a29
G Sasi Varunan <70020919+sasivarunan390@users.noreply.github.com>
2025-05-27 21:29:05 +0530

    Update JwtUtils.java

--- springappuser/src/main/java/com/examly/springappuser/config/JwtUtils.java
+++ springappuser/src/main/java/com/examly/springappuser/config/JwtUtils.java
@@ -1,36 +1,44 @@
+
 package com.examly.springappuser.config;
 
 import java.util.Date;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtUtils {
-    private final String SECRET_KEY = "secret";
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
 
-    public String  extraUsername(String token){
-        return getClaims(token).getSubject();
-
+    public String getJwtSecret() {
+        return jwtSecret;
     }
 
-    public boolean validateToken(String token, UserDetails userDetails){
-        String username = extraUsername(token);
-        return username.equals(userDetails.getUsername());
-
+    public long getJwtExpiration() {
+        return jwtExpiration;
+    }
+    public String generateToken(String email){
+        return Jwts.builder().setSubject(email).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis()+jwtExpiration))
+        .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
     }
 
-    private Claims getClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    public String extractUsername(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
-
-    public String generateToken(UserDetails userDetails) {
-         return Jwts.builder().setSubject(userDetails.getUsername())
-         .setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis()+ 86400000))
-         .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }
+        catch(JwtException|IllegalArgumentException e){
+            return false;
+        }
     }
-}
+ }
