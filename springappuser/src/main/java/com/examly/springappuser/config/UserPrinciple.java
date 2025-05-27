e786db589478e1788451919864a4e79a41802729
G Sasi Varunan <70020919+sasivarunan390@users.noreply.github.com>
2025-05-27 21:33:31 +0530

    Update UserPrinciple.java

--- springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
+++ springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
@@ -2,6 +2,7 @@ package com.examly.springappuser.config;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,36 +10,63 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.examly.springappuser.model.User;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-
 public class UserPrinciple implements UserDetails {
-    
-    private User user;
-     
-    public Collection<? extends GrantedAuthority> getAuthorities(){
-        return List.of(new SimpleGrantedAuthority(("Role_" + user.getUserRole())));
+    private static final long serialVersionUID=1L;
+    private transient User user;
+    public UserPrinciple(User user){
+        this.user=Objects.requireNonNull(user,"User cannot be null");
     }
-
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getUserRole()));
+    }
+    @Override
     public String getPassword(){
         return user.getPassword();
     }
+    @Override
     public String getUsername(){
-        return user.getUsername();
+        return user.getEmail();
     }
-    public boolean isAccountNonExpired(){
-        return true;
-    }
+    @Override
     public boolean isAccountNonLocked(){
         return true;
     }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
     public boolean isCredentialsNonExpired(){
         return true;
     }
-    public boolean isEnabled() {
+    @Override
+    public boolean isEnabled(){
         return true;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserPrinciple other = (UserPrinciple) obj;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+    
 }
+
