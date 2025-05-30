fcc331c09ffca77828bfa69f2006aff674d8f9dd
G Sasi Varunan <70020919+sasivarunan390@users.noreply.github.com>
2025-05-30 11:59:45 +0530

    Update RequirementController.java

--- springapptrainer/src/main/java/com/examly/springapptrainer/controller/RequirementController.java
+++ springapptrainer/src/main/java/com/examly/springapptrainer/controller/RequirementController.java
@@ -3,6 +3,7 @@ package com.examly.springapptrainer.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,45 +22,47 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/requirement")
 @RequiredArgsConstructor
 public class RequirementController {
-    
+
     private final RequirementService requirementService;
 
     @PostMapping
+    @PreAuthorize("hasRole('Manager')")
     public ResponseEntity<Requirement> addRequirement(@RequestBody Requirement requirement) {
-       
         Requirement saveRequirement = requirementService.addRequirement(requirement);
         return ResponseEntity.status(201).body(saveRequirement);
     }
 
     @GetMapping("/{requirementId}")
-    public ResponseEntity<Requirement> getRequirementById(@PathVariable Long requirementId ) {
+    @PreAuthorize("hasRole('Manager')")
+    public ResponseEntity<Requirement> getRequirementById(@PathVariable Long requirementId) {
         return requirementService.getRequirementById(requirementId)
-        .map(ResponseEntity::ok)
-        .orElse(ResponseEntity.notFound().build());
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    
     @GetMapping
-    public ResponseEntity<List<Requirement>> getAllRequirements(){
+    @PreAuthorize("hasAnyRole('Manager', 'Coordinator')")
+    public ResponseEntity<List<Requirement>> getAllRequirements() {
         return ResponseEntity.ok(requirementService.getAllRequirements());
     }
 
     @PutMapping("/{requirementId}")
-    public ResponseEntity<Requirement> updateRequirement(@PathVariable Long requirementId,@RequestBody Requirement requirement ) {
-        Requirement updateRequirement = requirementService.updateRequirement(requirementId,requirement);
+    @PreAuthorize("hasRole('Manager')")
+    public ResponseEntity<Requirement> updateRequirement(@PathVariable Long requirementId, @RequestBody Requirement requirement) {
+        Requirement updateRequirement = requirementService.updateRequirement(requirementId, requirement);
         return ResponseEntity.ok(updateRequirement);
     }
 
     @DeleteMapping("/{requirementId}")
+    @PreAuthorize("hasRole('Manager')")
     public ResponseEntity<Void> deleteRequirement(@PathVariable Long requirementId) {
         requirementService.deleteRequirement(requirementId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/trainer/{trainerId}")
+    @PreAuthorize("hasAnyRole('Manager', 'Coordinator')")
     public ResponseEntity<List<Requirement>> getRequirementsByTrainerId(@PathVariable Long trainerId) {
         return ResponseEntity.ok(requirementService.getRequirementsByTrainerId(trainerId));
     }
-
-    
 }
