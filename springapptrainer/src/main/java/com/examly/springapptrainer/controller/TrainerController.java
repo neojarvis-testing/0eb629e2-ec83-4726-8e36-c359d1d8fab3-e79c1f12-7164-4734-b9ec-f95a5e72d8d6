c90461f8b2d9d8bb3a03728d3eae104a70bfb35d
G Sasi Varunan <70020919+sasivarunan390@users.noreply.github.com>
2025-05-30 12:00:13 +0530

    Update TrainerController.java

--- springapptrainer/src/main/java/com/examly/springapptrainer/controller/TrainerController.java
+++ springapptrainer/src/main/java/com/examly/springapptrainer/controller/TrainerController.java
@@ -1,8 +1,10 @@
 package com.examly.springapptrainer.controller;
 
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,37 +23,41 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/trainer")
 @RequiredArgsConstructor
 public class TrainerController {
-    
+
     private final TrainerService trainerService;
 
     @PostMapping
+    @PreAuthorize("hasRole('Coordinator')")
     public ResponseEntity<Trainer> addtrainer(@RequestBody Trainer trainer) {
         Trainer saveTrainer = trainerService.addTrainer(trainer);
         return ResponseEntity.status(201).body(saveTrainer);
     }
 
     @GetMapping("/{trainerId}")
-    public ResponseEntity<Trainer> getTrainerById(@PathVariable Long trainerId ) {
+    @PreAuthorize("hasAnyRole('Manager', 'Coordinator')")
+    public ResponseEntity<Trainer> getTrainerById(@PathVariable Long trainerId) {
         Trainer trainer = trainerService.getTrainerById(trainerId)
-                     .orElseThrow(()-> new IllegalArgumentException("Trainer not found"));
-        return ResponseEntity.ok(trainer);      
+                .orElseThrow(() -> new IllegalArgumentException("Trainer not found"));
+        return ResponseEntity.ok(trainer);
     }
 
     @GetMapping
-    public ResponseEntity<List<Trainer>> getAllTrainers(){
-         return ResponseEntity.ok(trainerService.getAllTrainers());
+    @PreAuthorize("hasAnyRole('Manager', 'Coordinator')")
+    public ResponseEntity<List<Trainer>> getAllTrainers() {
+        return ResponseEntity.ok(trainerService.getAllTrainers());
     }
 
     @PutMapping("/{trainerId}")
-    public ResponseEntity<Trainer> updateTrainer(@PathVariable Long trainerId,@RequestBody Trainer trainer ) {
-        Trainer updated = trainerService.updateTrainer(trainerId,trainer);
+    @PreAuthorize("hasRole('Coordinator')")
+    public ResponseEntity<Trainer> updateTrainer(@PathVariable Long trainerId, @RequestBody Trainer trainer) {
+        Trainer updated = trainerService.updateTrainer(trainerId, trainer);
         return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{trainerId}")
+    @PreAuthorize("hasRole('Coordinator')")
     public ResponseEntity<Void> deleteTrainer(@PathVariable Long trainerId) {
         trainerService.deleteTrainer(trainerId);
         return ResponseEntity.noContent().build();
     }
-
 }
