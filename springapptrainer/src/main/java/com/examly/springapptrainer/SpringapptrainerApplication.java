c93842b0d94c8051f2a6191d5736dff36e08296e
sasivarunan390 <sasivarunan390@gmail.com>
2025-05-30 06:18:20 +0000

    EnableDiscovery Updated

--- springapptrainer/src/main/java/com/examly/springapptrainer/SpringapptrainerApplication.java
+++ springapptrainer/src/main/java/com/examly/springapptrainer/SpringapptrainerApplication.java
@@ -3,7 +3,10 @@ package com.examly.springapptrainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringapptrainerApplication {
 
 	public static void main(String[] args) {
